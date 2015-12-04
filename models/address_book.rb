c484a434140e08aaa234c5103e27c6b5a1005bec
Kurt Gardner <kurtwgardner@gmail.com>
2015-12-04 16:42:17 -0700

    Created an Iterative Search

--- models/address_book.rb
+++ models/address_book.rb
@@ -31,6 +31,41 @@ class AddressBook
      end
    end
 
+#   # Search AddressBook for a specific entry by name
+#   def binary_search(name)
+     # #1 We save the index of the leftmost item in the array in a variable named lower, and the index of rightmost item in the array in upper
+#    lower = 0
+#    upper = @entries.length - 1
+
+    # #2 We loop while our lower index is less than or equal to our upper index.
+#    while lower <= upper
+      # #3 We find the middle index by taking the sum of lower and upper and dividing it by two.
+#      mid = (lower + upper) / 2
+#      mid_name = @entries[mid].name
+
+      # #4 We compare the name that we are searching for, name, to the name of the middle index, mid_name
+#      if name == mid_name
+#        return @entries[mid]
+#      elsif name < mid_name
+#        upper = mid - 1
+#      elsif name > mid_name
+#        lower = mid + 1
+#      end
+#    end
+
+#     return nil
+#   end
+
+   def iterative_search(name)
+     @entries.each do |existing_entry|
+       if name == existing_entry.name
+         return existing_entry
+       end
+     end
+
+     return nil
+   end
+
    def remove_entry(name, phone_number, email)
      # 1. find the index, if any, where we have a matching entry
      # 2. if index exists, delete it
